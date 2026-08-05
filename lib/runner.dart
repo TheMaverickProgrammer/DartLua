76dce0d7b34d18cf4a68ff65c315a85d68633c38
TheMaverickProgrammer <91709+TheMaverickProgrammer@users.noreply.github.com>
2026-08-04 20:25:48 -0500

    runner no longer prints to console by default. this way it does not pollute other projects.

--- lib/runner.dart
+++ lib/runner.dart
@@ -9,36 +9,32 @@ export 'package:puredartlua/lua/lua.dart';
 export 'package:puredartlua/lua/visitors/runtime/luaobject.dart';
 export 'dart:io';
 
-/// Prints [errs] using [verb] for readability.
-bool displayStdErr(List<String> errs, {required String verb}) {
-  for (final String s in errs) {
-    print(s);
-  }
-
-  if (errs.isNotEmpty) {
-    print('${errs.length} errors while $verb script.');
-    return true;
-  }
-
-  return false;
-}
+typedef RunnerErrorsCallback = void Function(List<String>);
 
 /// Reads a [File] at [path] and calls [parse].
-AST? parseFile(String path) =>
-  parse(File(path).readAsStringSync());
+/// Forwards [onErrors] to [parse].
+AST? parseFile(String path, {RunnerErrorsCallback? onErrors}) =>
+    parse(File(path).readAsStringSync(), onErrors: onErrors);
 
 /// Reads the lua file at [path].
-/// If there was an error, calls [displayStdErr] an returns null.
+/// If there was an error, calls [onErrors] with those errors
+/// and returns null.
 /// Otherwise returns the constructed [AST].
-AST? parse(String content) {
+AST? parse(String content, {RunnerErrorsCallback? onErrors}) {
   final Lexer lexer = Lexer.tokenize(content)..dropComments();
 
-  if (displayStdErr(lexer.errors, verb: 'tokenizing')) return null;
+  if (lexer.errors.isNotEmpty) {
+    onErrors?.call(lexer.errors);
+    return null;
+  }
 
   final Parser parser = Parser(lexer.tokens);
   final ast = parser.analyze();
 
-  if (displayStdErr(parser.errors, verb: 'parsing')) return null;
+  if (parser.errors.isNotEmpty) {
+    onErrors?.call(parser.errors);
+    return null;
+  }
 
   return ast;
 }
@@ -47,18 +43,30 @@ AST? parse(String content) {
 /// Then passes [ast] into [EvaluatorMixin.visitAST] to parse
 /// and evaluate the scriptlet or program.
 ///
-/// If there was an error returns false.
-/// On complete, returns a tuple [(bool, LuaObject)] containing the result, if any.
+/// Returns a tuple [(bool, LuaObject)] containing the result, if any.
+/// If there was an error returns a tuple [(false, LuaObject.nil)].
+/// Additionally, the [onErrors] callback will be invoked with those errors.
 /// If the result would be [null], then returns an instance of [LuaObject.nil].
-(bool, LuaObject) runner(AST ast, {required EvaluatorMixin Function() constructor}) {
+(bool, LuaObject) runner(
+  AST ast, {
+  required EvaluatorMixin Function() constructor,
+  RunnerErrorsCallback? onErrors,
+}) {
   final eval = constructor.call();
   final out = eval.visitAST(ast);
 
-  if (displayStdErr(eval.errors, verb: 'running')) return (false, LuaObject.nil('out'));
+  if (eval.errors.isNotEmpty) {
+    onErrors?.call(eval.errors);
+    return (false, LuaObject.nil('out'));
+  }
 
   return (true, out?.toLua('out') ?? LuaObject.nil('out'));
 }
 
 /// Implementation over [runner] uses [Evaluator] machinery for the runtime.
-(bool, LuaObject) run(AST ast) => runner(ast, constructor: () => Evaluator());
-
+/// Uses [print] for the default [RunnerErrorsCallback] value if not provided.
+(bool, LuaObject) run(AST ast, {RunnerErrorsCallback? onErrors}) => runner(
+  ast,
+  constructor: () => Evaluator(),
+  onErrors: onErrors ?? (errs) => errs.forEach(print),
+);
